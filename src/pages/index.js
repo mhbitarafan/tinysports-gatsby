663861d30aa61dafefab633d38d7b73c047b69b0
mhbitarafan <mhbitarafan1375@gmail.com>
2019-08-23 22:28:17 +0430

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () => (
   <div style={{ color: `purple` }}>
     <Link to="/contact/">Contact</Link>
     <Header headerText="Hello Gatsby!" />
-    <p>What a world.</p>
+    <p>Gatsby is awesome</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )
